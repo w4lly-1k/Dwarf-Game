343bba659b5a9f8fb4e9dd1e1eec3a2b1571eef6
7xft <7xfttt@gmail.com>
2023-07-26 19:45:57 +1000

    Added enemy behaviour and dying code

--- Dwarf Game/Assets/Script/Enemy/Enemy.cs
+++ Dwarf Game/Assets/Script/Enemy/Enemy.cs
@@ -4,28 +4,62 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [Header("Paramaters")]
     [SerializeField] private int maxEnemyHP;
+    [SerializeField] private float viewDistance;
+    [SerializeField] private float moveSpeed;
 
+    private Vector2 playerPos;
+    private Vector2 playerDirection;
+    private bool hasSeen;
+    private Transform player;
+    private Rigidbody2D rb;
 
-    private bool hasHit;
-
-
+    public float damage;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-
 
     void Update()
     {
-        
+        if (playerDirection.x < 0)
+        {
+            MoveLeft();
+        }
+        else if (playerDirection.x > 0)
+        {
+            MoveRight();
+        }
+
+        if (player != null)
+        {
+            playerPos = player.transform.position;
+            playerDirection = (player.transform.position - transform.position).normalized;
+        }
     }
 
-    private void Hit()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-     
+        if (collision.tag == "Player")
+        {
+            hasSeen = true;
+            player = collision.GetComponent<Transform>();
+
+        }
     }
+
+
+    private void MoveLeft() 
+    {
+        rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+    }
+
+
+    private void MoveRight()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+
 }
---
+++ Dwarf Game/Assets/Script/Player/Combat/Health.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private Enemy enemy;
+
+    [SerializeField] private float maxHealth;
+    private float currentHealth;
+    
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        Die();
+        Debug.Log(currentHealth);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            enemy = collision.GetComponentInParent<Enemy>();           
+
+            if (enemy != null)
+            {
+                TakeDamage(enemy.damage);
+            }
+        }
+
+        
+
+    }
+
+    private void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+    }
+
+    private void Die()
+    {
+        if (currentHealth <= 0)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+
+    }
+}
